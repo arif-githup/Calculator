672b8016998ba21a290e947d8b0b044d17d430f3
arif <a.wibowo@students.amikom.ac.id>
2026-04-09 10:28:31 +0700

    pembuatan calculator

--- Calculator.cs
+++ Calculator.cs
@@ -2,8 +2,22 @@ class Calculator
 {
 	static void main (string[] args)
 	{
+		int a = 10;
+		int b = 6
 		
+		Console.WriteLine("Hasil Penambahan: {0} + {1} = {2}", a,b, penambahan(a, b));
+		Console.WriteLine("Hasil Pengurangan: {0} - {1} = {2}", a,b, pengurangan(a, b));
+		
+		Console.WriteLine("\nTekan sembarang key untuk keluar");
+		Console.Readkey();
 	}
-	static init penambahan(int a, int b){
+	static init penambahan(int a, int b)
+	{
 		return a + b;
 	}
+	
+	statics int pengurangan(int a, int b)
+	{
+		return a - b;
+	}
+}
